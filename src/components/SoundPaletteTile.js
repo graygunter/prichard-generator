263077f9a5c1a534b0d9314eae55ca99af7293d5
Gray Gunter <gray.gunter@gmail.com>
2017-06-10 18:38:55 -0400

    Responsive redesign

--- src/components/SoundPaletteTile.js
+++ src/components/SoundPaletteTile.js
@@ -32,7 +32,10 @@ class SoundPaletteTile extends Component {
                     className="icon"
                     style={{backgroundImage: iconPath}}>    
               </div>
-              <div className="file">{fileName}</div>
+              <div className={
+                classNames("file", fileName.length > 16 ? "small" : null)}>
+                  {fileName}
+              </div>              
             </div>
     );
 
@@ -60,17 +63,26 @@ class SoundPaletteTile extends Component {
         onDragStart={() => this.props.handleSoundTileDrag(this)}
         onDragEnd={() => this.props.handleSoundTileDragEnd(this)}>
         
-          <button 
-                  className="sound-tile-play"
-                  onClick={() => this.props.handleSoundTilePlay(this.state.file)}>
-                    <i className="fa fa-play" aria-hidden="true"></i>
-          </button>
-          
-          <button 
-                  className="sound-tile-refresh"
-                  onClick={this.getRandomSound}>
-                    <i className="fa fa-refresh" aria-hidden="true"></i>
-          </button>
+          <div className="sound-tile-buttons">
+            
+            <button 
+                    className="sound-tile-play"
+                    onClick={() => this.props.handleSoundTilePlay(this.state.file)}>
+                      <i className="fa fa-play" aria-hidden="true"></i>
+            </button>
+            
+            <button 
+                    className="sound-tile-explore">
+                      <i className="fa fa-search" aria-hidden="true"></i>
+            </button>
+
+            <button 
+                    className="sound-tile-refresh"
+                    onClick={this.getRandomSound}>
+                      <i className="fa fa-refresh" aria-hidden="true"></i>
+            </button>
+
+          </div>
           
           {this.state.file !== undefined ? this.createIconAndFile() : null}
 
