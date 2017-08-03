2fd1c97c6c5c164e092105c88d9f8eb7993dcbc8
Gray Gunter <gray.gunter@gmail.com>
2017-08-02 22:12:02 -0400

    Began Sound Tile Explore File and new audio
    Have completed through episode 11. Still need to add all the andre mp3s to the audio JSON Object.

---
+++ src/components/SoundTileExploreItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class SoundTileExploreItem extends Component {
+
+  render() {
+
+    return (
+
+      <div className="explore-item">
+
+        <div className="explore-item-name"></div>
+
+        <div className="explore-item-select"></div>
+
+        <div className="explore-item-tags"></div>
+
+      </div>
+    );
+
+  }
+
+}
+
+export default SoundTileExploreItem;
