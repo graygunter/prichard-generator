ad3be30f5db5feecd3d5dfb47e0164da17cbcb6e
Gray Gunter <gray.gunter@gmail.com>
2018-02-11 22:32:57 -0500

    Continued revisions to the About Screen

--- src/components/AboutScreen.js
+++ src/components/AboutScreen.js
@@ -24,10 +24,10 @@ class AboutScreen extends Component {
           </h2>
 
           <p>
-            <span className="firstcharacter">H</span>ave you ever found yourself wanting more of the <a href="http://www.mlwradio.com/something-to-wrestle-with-bruce-prichard.html" target="_blank">Something to Wrestle with Bruce Prichard</a> podcast, but it’s not noon on a Friday and the hands on the clock keep spinning too slow? Have you ever heard <a href="https://twitter.com/HeyHeyItsConrad" target="_blank">Conrad Thompson</a> ask <a href="https://twitter.com/bruceprichard" target="_blank">Bruce</a> a question and thought “I know exactly which catch phrase he’s going to respond with”? 
+            <span className="firstcharacter">H</span>ave you ever found yourself wanting more of the <a href="http://www.mlwradio.com/something-to-wrestle-with-bruce-prichard.html" target="_blank">Something to Wrestle with Bruce Prichard</a> podcast, but it’s not noon on a Friday and the hands on the clock keep spinning too slow? Have you ever heard <a href="https://twitter.com/HeyHeyItsConrad" target="_blank">Conrad Thompson</a> ask <a href="https://twitter.com/bruceprichard" target="_blank">Bruce</a> a question and thought “I know exactly which catch phrase or impression he’s going to respond with”? 
           </p>
           <p>
-            If you answered yes to any of the above you're a huge nerd, but now with the power of <strong>Something to Generate Something to Wrestle with Bruce Prichard</strong> you’re a nerd who can do something about it, brother.
+            If you answered 'yes' to any of the above you're a huge mark, but now with the power of <strong>Something to Generate Something to Wrestle with Bruce Prichard</strong> you’re a mark who can do something about it, brother.
           </p>
 
           <h2>
@@ -42,7 +42,7 @@ class AboutScreen extends Component {
                     src={require(`../imgs/instructions/random_button.gif`)} />
 
               <p>
-                Click the <strong>random button</strong> on any of the <strong>{this.props.numCategories} categories</strong> to hear a random audio clip from that category. In this example we're about to play a random audio clip from the category <strong>Vince</strong>. But the fun doesn’t stop there...
+                Click the <strong>random button</strong> on any of the <strong>{this.props.numCategories} categories</strong> to hear a random audio clip from that category. {/* In this example we're about to play a random audio clip from the category <strong>Vince</strong>. But the fun doesn’t stop there...*/}
               </p>
             </li>
 
@@ -52,7 +52,7 @@ class AboutScreen extends Component {
                     src={require(`../imgs/instructions/explore_button.gif`)} />
 
               <p>
-                Click the <strong>explore button</strong> on a category to view every single audio clip associated with the category. In this example we're about to explore the category <strong>Baby Tonight</strong>.
+                Click the <strong>explore button</strong> on a category to view every single audio clip associated with the category. {/* In this example we're about to explore the category <strong>Baby Tonight</strong> */}
               </p>
             </li>
 
@@ -62,7 +62,7 @@ class AboutScreen extends Component {
                     src={require(`../imgs/instructions/explore_screen.gif`)} />
 
               <p>
-                Clicking the <strong>explore button</strong> brings up the <strong>explore screen</strong> a listing of every single audio clip in the category. Scroll to the bottom of the page to see them all. In this example we're scrolling through the category <strong>Conrad</strong>.
+                Clicking the <strong>explore button</strong> brings up the <strong>explore screen</strong> a listing of every audio clip in a category. Scroll to the bottom of the page to see them all. {/* In this example we're scrolling through the category <strong>Conrad</strong>*/}.
               </p>
             </li>
 
@@ -72,7 +72,7 @@ class AboutScreen extends Component {
                     src={require(`../imgs/instructions/audio_clip.gif`)} />
 
               <p>
-                Each <strong>audio clip</strong> is represented by a rectangle on the <strong>explore screen</strong>. The large text in the top left is the <strong>title</strong> of the audio clip. The blue <strong>play button</strong> plays the audio clip. The green <strong>add button</strong> will add that audio clip to the <strong>playlist</strong>. In the bottom left are <strong>tags</strong> associated with the audio clip separated by #. In this example we’re looking at the audio clip <strong>rolltide10</strong> which is tagged “roll tide”, “on that” and “indeed”.
+                Each <strong>audio clip</strong> is represented by a rectangle. The large text in the top left is the <strong>title</strong>. The blue <strong>play button</strong> plays the audio clip. The green <strong>add button</strong> will add that audio clip to the <strong>playlist</strong>. In the bottom left are <strong>tags</strong> associated with the audio clip separated by #. {/* In this example we’re looking at the audio clip <strong>rolltide10</strong> which is tagged “#roll tide”, “#on that” and “#indeed”.*/}
               </p>
             </li>
 
@@ -92,7 +92,7 @@ class AboutScreen extends Component {
                     src={require(`../imgs/instructions/filter.gif`)} />
 
               <p>
-                You can filter which audio clips appear by their tags. Click the <strong>filter by tag</strong> input and start typing. In this example we're in the category Bruce and filtering out any audio files that don't have the tag <strong>drugs</strong>.
+                You can filter which audio clips appear by their tags. Click the <strong>filter by tag</strong> input and start typing. {/* In this example we're in the category Bruce and filtering out any audio files that don't have the tag <strong>drugs</strong>.*/}
               </p>
             </li>
 
@@ -123,24 +123,27 @@ class AboutScreen extends Component {
           </h2>
 
           <p>
-            STGSTW is a ReactJS application, powered by NodeJS and styled by SASS. Development began at the end of April 2017. The project is available publicly on Github.
+            STGSTW is a <a href="https://reactjs.org/" target="_blank">ReactJS</a> application, powered by <a href="https://nodejs.org/en/" target="_blank">NodeJS</a> and styled by <a href="https://sass-lang.com/" target="_blank">SASS</a>. Development began in May 2017. The project is available publicly on <a href="https://github.com/graygunter/prichard-generator" target="_blank">Github</a>.
           </p>
 
-          <img 
-                alt="ReactJS"
-                className="react"
-                src={require(`../imgs/logos/reactjs.png`)} />
+          <div className="logos">
 
-          <img 
-                alt="NodeJS"
-                className="node"
-                src={require(`../imgs/logos/nodejs.png`)} />
+            <img 
+                  alt="ReactJS"
+                  className="react"
+                  src={require(`../imgs/logos/reactjs.png`)} />
 
-          <img 
-                alt="SASS"
-                className="sass"
-                src={require(`../imgs/logos/sass.svg`)} />
+            <img 
+                  alt="SASS"
+                  className="sass"
+                  src={require(`../imgs/logos/sass.svg`)} />
 
+            <img 
+                  alt="NodeJS"
+                  className="node"
+                  src={require(`../imgs/logos/nodejs.png`)} />
+
+          </div>
 
         </div>
 
